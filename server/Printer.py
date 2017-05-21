57bd4c1d55cdb5643a260d24c8fb2945094d187d
AlbaAdrien <adrien.sartori@alumni.univ-avignon.fr>
2017-05-21 23:21:09 +0200

    Modif Printer.py

--- server/Printer.py
+++ server/Printer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
 
+import re
+import ConfigParser
+import argparse
 
 def get_last_info(hostname):
 	con = sqlite.connect(Config.DB_FILE)
@@ -24,3 +27,33 @@ def get_hostnames():
     con = sqlite.connect(Config.DB_FILE)
     cur = con.cursor()
     return cur.execute("SELECT DISTINCT(hostname) FROM infosys ORDER BY hostname;").fetchall()
+    
+def print_informations(infos, porcelain):
+    config = read_config()
+    for option in config.items(SECTION):
+        if option[0] == 'separator' and not porcelain:
+            print re.sub('"', '', option[1])
+            continue
+        elif option[0] not in infos:
+            continue
+        info = re.sub('"', '', option[1].strip())
+        value = infos[option[0]]
+        if porcelain:
+            msg = '{} {}'.format(option[0], value)
+        else:
+            msg = '{0:<35} '
+            msg += '{1:>19}' if Colors.GREEN else '{1:>10}'
+            msg = msg.format(info, Colors.color(value, Colors.GREEN))
+        print msg
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description=DESCRIPTION)
+    parser.add_argument('-c', '--color', action="store_true", default=False,
+                        help="Affiche des couleurs")
+    parser.add_argument('-p', '--porcelain', action="store_true", default=False,
+                        help="Formate la sortie pour être facilement utilisée par un script")
+    args = vars(parser.parse_args())
+
+    # if args['color']:
+        # Colors.activate()
+
